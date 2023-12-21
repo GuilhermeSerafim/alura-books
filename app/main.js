f91fa5c1672be175760284ea9cc172a15cd1dff7
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-21 14:49:42 -0300

    Adicionando arquivos js e separando responsabilidades entre eles

--- app/main.js
+++ app/main.js
@@ -1,5 +1,3 @@
-const livrosDOM = document.getElementById('livros');
-
 // Responsável pela requisição da api
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaAPI();
@@ -9,22 +7,4 @@ async function getBuscarLivrosDaAPI() {
     livrosAPI = await res.json();
     console.table(livrosAPI);
     exibirLivros(livrosAPI)
-}
-
-function exibirLivros(livrosAPI) {
-    livrosAPI.forEach(livroAPI => {
-        livrosDOM.innerHTML += `
-        <div class="livro">
-            <img class="livro__imagens" src="${livroAPI.imagem}" alt="${livroAPI.alt}" />
-            <h2 class="livro__titulo">
-                ${livroAPI.titulo}
-            </h2>
-            <p class="livro__descricao">${livroAPI.autor}</p>
-            <p class="livro__preco" id="preco">${livroAPI.preco}</p>
-            <div class="tags">
-                <span class="tag">${livroAPI.categoria}</span>
-            </div>
-        </div>
-        `
-    });
 }
---
+++ app/metodoForeach.js
@@ -0,0 +1,20 @@
+const livrosDOM = document.getElementById('livros');
+
+function exibirLivros(livrosAPI) {
+    // Percorrendo livros na api e exibindo na pagina
+    livrosAPI.forEach(livroAPI => {
+        livrosDOM.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${livroAPI.imagem}" alt="${livroAPI.alt}" />
+            <h2 class="livro__titulo">
+                ${livroAPI.titulo}
+            </h2>
+            <p class="livro__descricao">${livroAPI.autor}</p>
+            <p class="livro__preco" id="preco">${livroAPI.preco}</p>
+            <div class="tags">
+                <span class="tag">${livroAPI.categoria}</span>
+            </div>
+        </div>
+        `
+    });
+}
