94b3ec84e53131e4258181cc8c75a533b2454baf
GuilhermeSerafim <guilerstudies@gmail.com>
2023-12-21 14:40:21 -0300

    Percorrendo livros da api e exibindo os mesmo na página dinamicamente

--- app/main.js
+++ app/main.js
@@ -1,10 +1,30 @@
-// Responsável pelas requisições api
+const livrosDOM = document.getElementById('livros');
+
+// Responsável pela requisição da api
 const endpointAPI = 'https://guilhermeonrails.github.io/casadocodigo/livros.json';
 getBuscarLivrosDaAPI();
 
 async function getBuscarLivrosDaAPI() {
     const res = await fetch(endpointAPI);
-    livros = await res.json();
-    console.log(livros);
-    
+    livrosAPI = await res.json();
+    console.table(livrosAPI);
+    exibirLivros(livrosAPI)
+}
+
+function exibirLivros(livrosAPI) {
+    livrosAPI.forEach(livroAPI => {
+        livrosDOM.innerHTML += `
+        <div class="livro">
+            <img class="livro__imagens" src="${livroAPI.imagem}" alt="${livroAPI.alt}" />
+            <h2 class="livro__titulo">
+                ${livroAPI.titulo}
+            </h2>
+            <p class="livro__descricao">${livroAPI.autor}</p>
+            <p class="livro__preco" id="preco">${livroAPI.preco}</p>
+            <div class="tags">
+                <span class="tag">${livroAPI.categoria}</span>
+            </div>
+        </div>
+        `
+    });
 }
