3a461302ce940ef2c450d935b5e0737214c298e3
GuilhermeSerafim <guilerstudies@gmail.com>
2024-01-03 11:20:58 -0300

    Entendendo como o sort funciona com string

--- app/main.js
+++ app/main.js
@@ -10,12 +10,36 @@ async function getBuscarLivrosDaAPI() {
     exibirLivros(livrosComDescontoAplicado);
 }
 
-var x = [1, 3, 2];
-x.sort((a, b) => {
-    console.log(a)
-    console.log(b)
-    console.log("---")
-    return a - b;
-})
+var strings = ["banana", "maçã", "abacaxi", "uva"];
 
-console.log("Ordenação crescente: " + x);
+// Retornar 0, deixa a e b inalterados em relação um ao outro
+console.log("Ordenação iguais")
+strings.sort(() => {
+    return 0;
+});
+console.log(strings);
+
+console.log("Alfabética (padrão)")
+strings.sort();
+strings.sort(() => {
+    return 1;
+});
+console.log(strings);
+
+console.log("Ordenação contrária")
+strings.sort(() => {
+    return -1;
+});
+console.log(strings);
+
+// strings.sort((a, b) => {
+//     if (a é menor que b em algum critério de ordenação) {
+//         return -1;
+//       }
+//       if (a é maior que b em algum critério de ordenação) {
+//         return 1;
+//       }
+//       if(a deve ser igual a b) {
+//           return 0;
+//       }
+// })
